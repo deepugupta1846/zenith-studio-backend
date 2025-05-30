25264db94d3f8fa9c3e5b63df53c3c3875bc81e3
Deepu kumar <dk251248@gmail.com>
2025-05-31 03:41:03 +0530

    [added] nodemailer and usertype

--- models/User.js
+++ models/User.js
@@ -1,15 +1,35 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: "user" },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  licenseKey: {
+    type: String,
+    default: "",
+  },
+  userType: {
+    type: String,
+    enum: ["user", "admin", "retailer", "distributor"],
+    default: "user",
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+}, { timestamps: true });
+
 
 // Encrypt password before saving
 userSchema.pre("save", async function (next) {
