acb8d681a202ce81fa406738423e46bccdef32e0
Deepu Kumar <dk251248@gmail.com>
2025-10-26 05:02:40 +0530

    update bacjuo routes

--- routes/backupRoutes.js
+++ routes/backupRoutes.js
@@ -1,24 +1,24 @@
 import express from "express";
-import {
-  createBackup,
-  getBackupStats,
-  downloadBackup,
-  deleteBackup
-} from "../controllers/backupController.js";
+// import {
+//   createBackup,
+//   getBackupStats,
+//   downloadBackup,
+//   deleteBackup
+// } from "../controllers/backupController.js";
 
 const router = express.Router();
 
-// Create a new backup
-router.post("/create", createBackup);
+// // Create a new backup
+// router.post("/create", createBackup);
 
-// Get backup statistics and list
-router.get("/stats", getBackupStats);
+// // Get backup statistics and list
+// router.get("/stats", getBackupStats);
 
-// Download a specific backup file
-router.get("/download/:filename", downloadBackup);
+// // Download a specific backup file
+// router.get("/download/:filename", downloadBackup);
 
-// Delete a specific backup file
-router.delete("/delete/:filename", deleteBackup);
+// // Delete a specific backup file
+// router.delete("/delete/:filename", deleteBackup);
 
 export default router;
 
